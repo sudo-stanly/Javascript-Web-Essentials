19e3c27cb50f07ebe8a2ed75cebec2f61065a5ef
sudo-stanly <alys.git24@gmail.com>
2025-12-25 11:22:22 +0800

    querySelector

---
+++ Dom/index.js
@@ -0,0 +1,7 @@
+
+import Dom from "./part1.js";
+let dom = new Dom();
+
+window.onload = () => {
+    dom.changeContent(".message");
+}
---
+++ Dom/part1.js
@@ -0,0 +1,16 @@
+
+export default class Dom{
+    constructor(){
+        this.message = "The content has been changed.";
+        this.color="blue";
+        this.color2="red";
+    }
+    changeContent(classId){
+        let x = document.querySelector(classId);
+        if(x){ x.textContent=this.message; }
+        return x;
+    }
+
+    
+    
+}
