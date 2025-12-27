0ce505918fd45244b1acc9c09648901174415b63
sudo-stanly <alys.git24@gmail.com>
2025-12-27 21:53:35 +0800

    querySelectorAll demo functions

--- sandbox/index.js
+++ sandbox/index.js
@@ -16,7 +16,53 @@ function changeBackground(){
 
 function changeTheme() {
     document.querySelector("body").classList.toggle("dark-theme");
-    document.querySelector("article").classList.toggle("dark-theme");
-    document.querySelector(".card").classList.toggle("dark-theme");
+    let article = document.querySelectorAll("article");
+    article.forEach(article=>{
+        article.classList.toggle("dark-theme");
+    });
+    let card = document.querySelectorAll(".card");
+    card.forEach(card => {
+        card.classList.toggle("dark-theme");
+    });
+    return;
+}
+
+function changeSelectedElements(){
+    let texts = document.querySelectorAll(".text");
+    texts.forEach((text) => {
+        text.classList.add("background");
+    });
+    return;
+}
+
+function changeSelectedElements2() {
+    let nodeList = document.querySelectorAll(".span-tag");
+    nodeList[0].classList.add("background");
+    return;
+}
+
+function checkElementsLength(){
+    let nodeList = document.querySelectorAll(".span-tag2");
+    let demo = document.getElementById("demo");
+    demo.innerHTML = `The length is: ${nodeList.length}`;
+    demo.classList.add("background");
+    return;
+}
+
+function changeAllElements(){
+    let nodeList = document.querySelectorAll("div > p");
+    for(let i=0; i < nodeList.length; i++){
+        nodeList[i].style.backgroundColor="red";
+        nodeList[i].style.color = "white";
+    }
+    return;
+}
+
+function changeAllElements() {
+    let nodeList = document.querySelectorAll("h3, span");
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].style.backgroundColor = "red";
+        nodeList[i].style.color = "white";
+    }
     return;
 }
